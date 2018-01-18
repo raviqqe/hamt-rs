eccd9793b8a14004e821d67557179e30a4dee338
Yota Toyama <raviqqe@gmail.com>
2018-01-18 11:43:50 +0900

    Add Entry enum

--- src/hamt.rs
+++ src/hamt.rs
@@ -1,17 +1,34 @@
 use std::sync::Arc;
 
+use bucket::Bucket;
+
+use self::Entry::*;
+
+enum Entry<K> {
+    Empty,
+    Key(K),
+    Hamt(Hamt<K>),
+    Bucket(Bucket<K>),
+}
+
+impl<K> Default for Entry<K> {
+    fn default() -> Self {
+        Empty
+    }
+}
+
 pub struct Hamt<K>(Arc<Inner<K>>);
 
 struct Inner<K> {
     bitmap: i32,
-    entries: [Option<K>; 32],
+    entries: [Entry<K>; 32],
 }
 
 impl<K> Hamt<K> {
     fn new(k: K) -> Self {
-        let mut es: [Option<K>; 32] = Default::default();
+        let mut es: [Entry<K>; 32] = Default::default();
 
-        es[0] = Some(k);
+        es[0] = Key(k);
 
         Hamt(Arc::new(Inner {
             bitmap: 0,
