fc4272c8eb5a5919717baeaf128f51617291e5ac
Yota Toyama <raviqqe@gmail.com>
2021-11-03 03:53:46 +0000

    Refactor

--- src/key_value.rs
+++ src/key_value.rs
@@ -1,20 +1,14 @@
-use crate::utilities::hash_key;
 use std::hash::Hash;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct KeyValue<K, V> {
     key: K,
     value: V,
-    hash: u64,
 }
 
 impl<K: Hash, V> KeyValue<K, V> {
     pub fn new(key: K, value: V) -> Self {
-        Self {
-            hash: hash_key(&key),
-            key,
-            value,
-        }
+        Self { key, value }
     }
 }
 
@@ -25,9 +19,5 @@ impl<K, V> KeyValue<K, V> {
 
     pub fn value(&self) -> &V {
         &self.value
-    }
-
-    pub fn hash(&self) -> u64 {
-        self.hash
     }
 }
