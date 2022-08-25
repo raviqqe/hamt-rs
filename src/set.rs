e6154e769a3361d697c9473fb1b43a04d1922402
Yota Toyama <raviqqe@gmail.com>
2022-08-25 05:38:26 +0000

    Update

--- src/set.rs
+++ src/set.rs
@@ -1,18 +1,18 @@
 use crate::hamt::{Hamt, HamtIterator};
-use std::{borrow::Borrow, hash::Hash, ops::Index, sync::Arc};
+use std::{borrow::Borrow, hash::Hash, sync::Arc};
 
-/// Map data structure of HAMT.
+/// Set data structure of HAMT.
 ///
-/// Note that every method does not modify the original map but creates a new
+/// Note that every method does not modify the original set but creates a new
 /// one if necessary.
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Map<K, V> {
+pub struct Set<T> {
     size: usize,
-    hamt: Arc<Hamt<K, V>>,
+    hamt: Arc<Hamt<T, ()>>,
 }
 
-impl<K: Hash + Eq, V> Map<K, V> {
-    /// Creates a new map.
+impl<T: Hash + Eq> Set<T> {
+    /// Creates a new set.
     pub fn new() -> Self {
         Self {
             size: 0,
@@ -20,27 +20,19 @@ impl<K: Hash + Eq, V> Map<K, V> {
         }
     }
 
-    /// Finds a key and its corresponding value in a map.
-    pub fn get<Q: Hash + Eq + ?Sized>(&self, key: &Q) -> Option<&V>
+    /// Checks if a value is contained in a set.
+    pub fn contains<Q: Hash + Eq + ?Sized>(&self, value: &Q) -> bool
     where
-        K: Borrow<Q>,
+        T: Borrow<Q>,
     {
-        self.hamt.get(key)
-    }
-
-    /// Checks if a key is contained in a map.
-    pub fn contains_key<Q: Hash + Eq + ?Sized>(&self, key: &Q) -> bool
-    where
-        K: Borrow<Q>,
-    {
-        self.get(key).is_some()
+        self.hamt.get(value).is_some()
     }
 }
 
-impl<K: Clone + Hash + Eq, V: Clone> Map<K, V> {
-    /// Inserts a key-value pair into a map.
-    pub fn insert(&self, key: K, value: V) -> Self {
-        let (hamt, ok) = self.hamt.insert(key, value);
+impl<T: Clone + Hash + Eq> Set<T> {
+    /// Inserts a key-value pair into a set.
+    pub fn insert(&self, value: T) -> Self {
+        let (hamt, ok) = self.hamt.insert(value, ());
 
         Self {
             size: self.size + (ok as usize),
@@ -48,12 +40,12 @@ impl<K: Clone + Hash + Eq, V: Clone> Map<K, V> {
         }
     }
 
-    /// Removes a key and returns its corresponding value from a map if any.
-    pub fn remove<Q: Hash + Eq + ?Sized>(&self, key: &Q) -> Self
+    /// Removes a key and returns its corresponding value from a set if any.
+    pub fn remove<Q: Hash + Eq + ?Sized>(&self, value: &Q) -> Self
     where
-        K: Borrow<Q>,
+        T: Borrow<Q>,
     {
-        if let Some(hamt) = self.hamt.remove(key) {
+        if let Some(hamt) = self.hamt.remove(value) {
             Self {
                 size: self.size - 1,
                 hamt: hamt.into(),
@@ -63,44 +55,38 @@ impl<K: Clone + Hash + Eq, V: Clone> Map<K, V> {
         }
     }
 
-    /// Extends a map with an iterator of key-value pairs.
-    pub fn extend(&self, iterator: impl IntoIterator<Item = (K, V)>) -> Self {
-        let mut map = self.clone();
+    /// Extends a set with an iterator of key-value pairs.
+    pub fn extend(&self, iterator: impl IntoIterator<Item = T>) -> Self {
+        let mut set = self.clone();
 
-        for (key, value) in iterator {
-            map = map.insert(key, value);
+        for value in iterator {
+            set = set.insert(value);
         }
 
-        map
+        set
     }
 
-    /// Returns a size of a map.
+    /// Returns a size of a set.
     pub fn len(&self) -> usize {
         self.size
     }
 
-    /// Returns true if a map is empty.
+    /// Returns true if a set is empty.
     pub fn is_empty(&self) -> bool {
         self.size == 0
     }
 
-    /// Returns keys in a map
-    pub fn keys(&self) -> impl Iterator<Item = &K> {
-        self.into_iter().map(|(key, _)| key)
+    /// Returns values in a set
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        self.into_iter()
     }
 
-    /// Returns keys in a map
-    pub fn values(&self) -> impl Iterator<Item = &V> {
-        self.into_iter().map(|(_, value)| value)
-    }
-
-    /// Removes the first element in a map and returns a new map containing the
+    /// Removes the first element in a set and returns a new set containing the
     /// rest of elements.
-    pub fn first_rest(&self) -> Option<(&K, &V, Self)> {
-        self.hamt.first_rest().map(|(key, value, hamt)| {
+    pub fn first_rest(&self) -> Option<(&T, Self)> {
+        self.hamt.first_rest().map(|(key, _, hamt)| {
             (
                 key,
-                value,
                 Self {
                     size: self.size - 1,
                     hamt: hamt.into(),
@@ -110,30 +96,19 @@ impl<K: Clone + Hash + Eq, V: Clone> Map<K, V> {
     }
 }
 
-impl<K: Clone + Hash + Eq, V: Clone> Default for Map<K, V> {
+impl<T: Clone + Hash + Eq> Default for Set<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<Q: Hash + Eq + ?Sized, K: Hash + Eq, V> Index<&Q> for Map<K, V>
-where
-    K: Borrow<Q>,
-{
-    type Output = V;
-
-    fn index(&self, key: &Q) -> &V {
-        self.get(key).expect("invalid key")
-    }
-}
-
-impl<K: Clone + Hash + Eq, V: Clone> FromIterator<(K, V)> for Map<K, V> {
-    fn from_iter<T: IntoIterator<Item = (K, V)>>(iterator: T) -> Self {
+impl<T: Clone + Hash + Eq> FromIterator<T> for Set<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iterator: I) -> Self {
         let mut size = 0;
         let mut hamt = Hamt::new();
 
-        for (key, value) in iterator {
-            size += hamt.insert_mut(key, value) as usize;
+        for value in iterator {
+            size += hamt.insert_mut(value, ()) as usize;
         }
 
         Self {
@@ -143,186 +118,179 @@ impl<K: Clone + Hash + Eq, V: Clone> FromIterator<(K, V)> for Map<K, V> {
     }
 }
 
-pub struct MapIterator<'a, K: 'a, V: 'a>(HamtIterator<'a, K, V>);
+pub struct SetIterator<'a, T: 'a>(HamtIterator<'a, T, ()>);
 
-impl<'a, K, V> Iterator for MapIterator<'a, K, V> {
-    type Item = (&'a K, &'a V);
+impl<'a, T> Iterator for SetIterator<'a, T> {
+    type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.next()
+        self.0.next().map(|(key, _)| key)
     }
 }
 
-impl<'a, K, V> IntoIterator for &'a Map<K, V> {
-    type IntoIter = MapIterator<'a, K, V>;
-    type Item = (&'a K, &'a V);
+impl<'a, T> IntoIterator for &'a Set<T> {
+    type IntoIter = SetIterator<'a, T>;
+    type Item = &'a T;
 
     fn into_iter(self) -> Self::IntoIter {
-        MapIterator(self.hamt.into_iter())
+        SetIterator(self.hamt.into_iter())
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::Map;
+    use super::Set;
     use rand::{random, seq::SliceRandom, thread_rng};
     use std::thread::spawn;
 
-    const NUM_ITERATIONS: usize = 1 << 12;
+    const ITERATION_COUNT: usize = 1 << 12;
 
     #[test]
     fn new() {
-        Map::<u8, u8>::new();
+        Set::<u8>::new();
     }
 
     #[test]
     fn insert() {
-        let map = Map::new();
+        let set = Set::new();
 
-        assert_eq!(map.len(), 0);
-        assert_eq!(map.insert(0, 0).len(), 1);
-        assert_eq!(map.insert(0, 0).insert(0, 0).len(), 1);
-        assert_eq!(map.insert(0, 0).insert(1, 0).len(), 2);
+        assert_eq!(set.len(), 0);
+        assert_eq!(set.insert(0).len(), 1);
+        assert_eq!(set.insert(0).insert(0).len(), 1);
+        assert_eq!(set.insert(0).insert(1).len(), 2);
     }
 
     #[test]
     fn insert_many_in_order() {
-        let mut map = Map::new();
+        let mut set = Set::new();
 
-        for index in 0..NUM_ITERATIONS {
-            map = map.insert(index, index);
-            assert_eq!(map.len(), index + 1);
+        for index in 0..ITERATION_COUNT {
+            set = set.insert(index);
+            assert_eq!(set.len(), index + 1);
         }
     }
 
     #[test]
     fn insert_many_at_random() {
-        let mut map: Map<usize, usize> = Map::new();
+        let mut set: Set<usize> = Set::new();
 
-        for index in 0..NUM_ITERATIONS {
-            let ey = random();
-            map = map.insert(ey, ey);
-            assert_eq!(map.len(), index + 1);
+        for index in 0..ITERATION_COUNT {
+            let value = random();
+            set = set.insert(value);
+            assert_eq!(set.len(), index + 1);
         }
     }
 
     #[test]
     fn remove() {
-        let map = Map::new();
+        let set = Set::new();
 
-        assert_eq!(map.insert(0, 0).remove(&0), map.clone());
-        assert_eq!(map.insert(0, 0).remove(&1), map.insert(0, 0));
-        assert_eq!(map.insert(0, 0).insert(1, 0).remove(&0), map.insert(1, 0));
-        assert_eq!(map.insert(0, 0).insert(1, 0).remove(&1), map.insert(0, 0));
-        assert_eq!(
-            map.insert(0, 0).insert(1, 0).remove(&2),
-            map.insert(0, 0).insert(1, 0)
-        );
+        assert_eq!(set.insert(0).remove(&0), set.clone());
+        assert_eq!(set.insert(0).remove(&1), set.insert(0));
+        assert_eq!(set.insert(0).insert(1).remove(&0), set.insert(1));
+        assert_eq!(set.insert(0).insert(1).remove(&1), set.insert(0));
+        assert_eq!(set.insert(0).insert(1).remove(&2), set.insert(0).insert(1));
     }
 
     #[test]
     fn insert_remove_many() {
-        let mut map: Map<i16, i16> = Map::new();
+        let mut set = Set::<i16>::new();
 
-        for _ in 0..NUM_ITERATIONS {
-            let key = random();
-            let size = map.len();
-            let found = map.get(&key).is_some();
+        for _ in 0..ITERATION_COUNT {
+            let value = random();
+            let size = set.len();
+            let found = set.contains(&value);
 
             if random() {
-                map = map.insert(key, key);
+                set = set.insert(value);
 
-                assert_eq!(map.len(), if found { size } else { size + 1 });
-                assert_eq!(map.get(&key), Some(&key));
+                assert_eq!(set.len(), if found { size } else { size + 1 });
+                assert!(set.contains(&value),);
             } else {
-                map = map.remove(&key);
+                set = set.remove(&value);
 
-                assert_eq!(map.len(), if found { size - 1 } else { size });
-                assert_eq!(map.get(&key), None);
+                assert_eq!(set.len(), if found { size - 1 } else { size });
+                assert!(set.contains(&value),);
             }
         }
     }
 
     #[test]
-    fn get() {
-        let map = Map::new();
+    fn contains() {
+        let set = Set::new();
 
-        assert_eq!(map.insert(0, 0).get(&0), Some(&0));
-        assert_eq!(map.insert(0, 0).get(&1), None);
-        assert_eq!(map.insert(1, 0).get(&0), None);
-        assert_eq!(map.insert(1, 0).get(&1), Some(&0));
-        assert_eq!(map.insert(0, 0).insert(1, 0).get(&0), Some(&0));
-        assert_eq!(map.insert(0, 0).insert(1, 0).get(&1), Some(&0));
-        assert_eq!(map.insert(0, 0).insert(1, 0).get(&2), None);
+        assert!(set.insert(0).contains(&0));
+        assert!(!set.insert(0).contains(&1));
+        assert!(!set.insert(1).contains(&0));
+        assert!(set.insert(1).contains(&1));
+        assert!(set.insert(0).insert(1).contains(&0));
+        assert!(set.insert(0).insert(1).contains(&1));
+        assert!(!set.insert(0).insert(1).contains(&2));
+    }
+
+    #[test]
+    fn contains_borrowed() {
+        assert!(Set::<String>::new()
+            .insert("foo".to_string())
+            .contains("foo"));
     }
 
     #[test]
     fn first_rest() {
-        let mut map: Map<i16, i16> = Map::new();
+        let mut set: Set<i16> = Set::new();
 
-        for _ in 0..NUM_ITERATIONS {
-            map = map.insert(random(), 0);
+        for _ in 0..ITERATION_COUNT {
+            set = set.insert(random());
         }
 
-        for _ in 0..map.len() {
-            let (key, _, rest) = map.first_rest().unwrap();
+        for _ in 0..set.len() {
+            let (value, rest) = set.first_rest().unwrap();
 
-            assert_eq!(rest.len(), map.len() - 1);
-            assert_eq!(rest.get(key), None);
+            assert_eq!(rest.len(), set.len() - 1);
+            assert!(!rest.contains(value));
 
-            map = rest;
+            set = rest;
         }
 
-        assert_eq!(map, Map::new());
+        assert_eq!(set, Set::new());
     }
 
     #[test]
     fn equality() {
         for _ in 0..8 {
-            let mut maps: [Map<i16, i16>; 2] = [Map::new(), Map::new()];
-            let mut inserted_keys: Vec<i16> = (0..NUM_ITERATIONS).map(|_| random()).collect();
-            let mut deleted_keys: Vec<i16> = (0..NUM_ITERATIONS).map(|_| random()).collect();
+            let mut sets: [Set<i16>; 2] = [Set::new(), Set::new()];
+            let mut inserted_values: Vec<i16> = (0..ITERATION_COUNT).map(|_| random()).collect();
+            let mut deleted_values: Vec<i16> = (0..ITERATION_COUNT).map(|_| random()).collect();
 
-            for map in maps.iter_mut() {
-                inserted_keys.shuffle(&mut thread_rng());
-                deleted_keys.shuffle(&mut thread_rng());
+            for set in sets.iter_mut() {
+                inserted_values.shuffle(&mut thread_rng());
+                deleted_values.shuffle(&mut thread_rng());
 
-                for key in &inserted_keys {
-                    *map = map.insert(*key, *key);
+                for value in &inserted_values {
+                    *set = set.insert(*value);
                 }
 
-                for key in &deleted_keys {
-                    *map = map.remove(key);
+                for value in &deleted_values {
+                    *set = set.remove(value);
                 }
             }
 
-            assert_eq!(maps[0], maps[1]);
+            assert_eq!(sets[0], sets[1]);
         }
     }
 
     #[test]
     fn send_and_sync() {
-        let map: Map<usize, usize> = Map::new();
-        spawn(move || map);
+        let set: Set<usize> = Set::new();
+        spawn(move || set);
 
-        let map: Map<String, String> = Map::new();
-        spawn(move || map);
+        let set: Set<String> = Set::new();
+        spawn(move || set);
     }
 
     #[test]
     fn extend() {
-        assert_eq!(
-            Map::<usize, usize>::new().insert(0, 0),
-            Map::new().extend([(0, 0)])
-        );
-    }
-
-    #[test]
-    fn index() {
-        assert_eq!(
-            Map::<String, usize>::new().insert("foo".to_string(), 42)["foo"],
-            42,
-        );
+        assert_eq!(Set::<usize>::new().insert(0), Set::new().extend([0]));
     }
 
     mod from_iterator {
@@ -330,51 +298,45 @@ mod test {
 
         #[test]
         fn collect_empty() {
-            assert_eq!(Map::<usize, usize>::new(), [].into_iter().collect());
+            assert_eq!(Set::<usize>::new(), [].into_iter().collect());
         }
 
         #[test]
         fn collect_one_element() {
-            assert_eq!(
-                Map::<usize, usize>::new().insert(0, 0),
-                [(0, 0)].into_iter().collect()
-            );
+            assert_eq!(Set::<usize>::new().insert(0), [0].into_iter().collect());
         }
 
         #[test]
         fn collect_two_elements() {
             assert_eq!(
-                Map::<usize, usize>::new().insert(0, 0).insert(1, 1),
-                [(0, 0), (1, 1)].into_iter().collect()
+                Set::<usize>::new().insert(0).insert(1),
+                [0, 1].into_iter().collect()
             );
         }
 
         #[test]
         fn collect_with_reversed_order() {
             assert_eq!(
-                Map::<usize, usize>::new().insert(0, 0).insert(1, 1),
-                [(1, 1), (0, 0)].into_iter().collect()
+                Set::<usize>::new().insert(0).insert(1),
+                [1, 0].into_iter().collect()
             );
         }
 
         #[test]
-        fn collect_duplicate_keys() {
-            assert_eq!(
-                Map::<usize, usize>::new().insert(0, 0),
-                [(0, 0), (0, 0)].into_iter().collect()
-            );
+        fn collect_duplicate_values() {
+            assert_eq!(Set::<usize>::new().insert(0), [0, 0].into_iter().collect());
         }
 
         #[test]
         fn collect_many_elements() {
-            let keys = (0..100).collect::<Vec<_>>();
-            let mut map = Map::<usize, usize>::new();
+            let values = (0..100).collect::<Vec<_>>();
+            let mut set = Set::<usize>::new();
 
-            for &key in &keys {
-                map = map.insert(key, key);
+            for &value in &values {
+                set = set.insert(value);
             }
 
-            assert_eq!(map, keys.into_iter().map(|key| (key, key)).collect());
+            assert_eq!(set, values.into_iter().collect());
         }
     }
 }
